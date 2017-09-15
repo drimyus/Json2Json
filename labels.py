8d848461363cf535c5b32934ec8f7a0085fd4f0d
atlantic <dreamyouth@engineer.com>
2017-09-16 03:32:24 +0800

    create new label data

--- labels.py
+++ labels.py
@@ -1,16 +1,31 @@
 
 def load_Labels_Table(txt_path):
 
-    categories = []
-    types = []
+    list = []
     with open(txt_path) as f:
+        cnt = 0
         for line in f:
-            [category, type] = line[:-1].split(':')
-            categories.append(category.lower().replace('"', ''))
-            types.append(type.lower().replace('"', ''))
+            cnt += 1
+            print(cnt, line)
 
-    return categories, types
+            [src_key, src_value, category, type, value, tags] = line[:-1].split(':')
+            category.lower().replace('"', '')
+            type.lower().replace('"', '')
+            value.lower().replace('"', '')
+            tags.lower().replace('"', '')
+
+            dic = {
+                'key': src_key,
+                'category': category,
+                'type': type,
+                'value': value,
+                'tags': tags
+            }
+
+            list.append(dic)
+
+    return list
 
 if __name__ == '__main__':
-    label_txt_path = "./key/labels.txt"
+    label_txt_path = "./keys/labels.txt"
     load_Labels_Table(label_txt_path)
